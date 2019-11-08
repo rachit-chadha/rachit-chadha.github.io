0cad3282c94d4f3971273bb0638519fbf89cdb5d
Rachit Chadha <rachitch@gmail.com>
2019-11-07 23:53:55 -0500

    lab_8

---
+++ lab_8/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/*
+ * The 'express.static' middleware provides some services Express can use to
+ * serve files from a directory - in this case, the 'public' subdirectory of
+ * this project.
+ *
+ * The 'app.use' function attaches middleware to our Express application, so
+ * that when the application is running, it can serve static files. In this
+ * case, we mount it over the entire app: any web request that GETs a path that
+ * exists in the 'public' directory will be handled by the middleware. The
+ * middleware also serves the 'index.html' file in a directory (if it exists)
+ * whenever a client requests the directory itself.
+ *
+ * The 'public' directory for this project, in turn, contains all the HTML,
+ * Javascript, and CSS files needed to run a simple chat client connected to
+ * this server. Accessing this server's root URL will serve 'public/index.html',
+ * which contains our chat client. This gives users an easy way to connect to
+ * the server and interact with other users.
+ *
+ * See also:
+ *  - Express: Serving static files in Express
+ *    https://expressjs.com/en/starter/static-files.html
+ *  - Express: express.static()
+ *    https://expressjs.com/en/4x/api.html#express.static
+ *  - Express: Using middleware
+ *    https://expressjs.com/en/guide/using-middleware.html
+ *  - Express: app.use()
+ *    https://expressjs.com/en/4x/api.html#app.use
+ */
+
+
+app.use(express.static('public'));
+
+// this is a single route, in the simplest possible format
+// the simplest format is not necessarily the best one.
+// this is, right now, an introduction to Callback Hell
+// but it is okay for a first-level example
+
+function inst_in_list(arg) {
+  let s = arg.length;
+  let x;
+  let i = 0;
+  let a = [];
+  for (x = 0; x < s; x ++){
+    if (arg[x].department === "College of Information Studies"){
+      a[i] = arg[x].course_id;
+      i ++;
+    }
+  }
+  return a;
+}
+
+app.get('/api', (req, res) => {
+  const baseURL = 'https://api.umd.io/v0/courses/list';
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      const final = data.filter(item => item.dept_id === "INST");
+      console.log(final);
+      res.send({ final: final });
+    })
+    //.then(r => inst_in_list(r))
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
